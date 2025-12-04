9f9f7d20fd99c9ee6ff259640b03c6b97c4c177b
Roma Bronstein <romabr@payoneer.com>
2025-12-04 23:36:38 +0200

    add IEvDBPayload

---
+++ apps/sample-app/src/index.ts
@@ -0,0 +1,12 @@
+import { IEvDbPayload } from '@eventualize/entities-types/IEvDbEventPayload';
+
+
+class Event1 implements IEvDbPayload {
+    readonly payloadType = 'event_1';
+    constructor(public readonly value1: number) { }
+}
+
+class Event2 implements IEvDbPayload {
+    readonly payloadType = 'event_2';
+    constructor(public readonly value2: number) { }
+}
