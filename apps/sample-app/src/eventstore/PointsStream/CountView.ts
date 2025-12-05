74ccc695febfe31d838e97433a7d8e00f4f4336a
Roma Bronstein <romabr@payoneer.com>
2025-12-06 01:19:12 +0200

    code cleanup

--- apps/sample-app/src/eventstore/PointsStream/CountView.ts
+++ apps/sample-app/src/eventstore/PointsStream/CountView.ts
@@ -19,29 +19,3 @@ export default createViewFactory<CountViewState, PointsStreamEvents>({
         }
     }
 });
-
-// export default class CountView extends EvDbView<CountViewState> implements IEvDbViewAppliesSet<CountViewState, PointsStreamEvents> {
-//     applyPointsAdded(oldState: CountViewState, newEvent: PointsAdded) {
-//         const newState = new CountViewState(oldState.count + 1);
-//         return newState;
-//     };
-//     applyPointsSubtracted(oldState: CountViewState, newEvent: PointsSubtracted, eventMetadata: IEvDbEventMetadata) { return new CountViewState(oldState.count + 1) };
-
-//     public getDefaultState(): CountViewState {
-//         return new CountViewState();
-//     }
-
-//     constructor(
-//         streamId: string,
-//         storageAdapter: IEvDbStorageSnapshotAdapter,
-//         storedAt: Date | undefined = undefined,
-//         snapshot: EvDbStoredSnapshotResult<CountViewState> = EvDbStoredSnapshotResult.getEmptyState<CountViewState>(),
-//         storeOffset: number = 0,
-//         memoryOffset: number = 0,
-//     ) {
-//         const streamAddress = new EvDbStreamAddress('ExampleStream', streamId);
-//         const viewAddress = new EvDbViewAddress(streamAddress, 'CountView');
-//         super(viewAddress, storedAt, storeOffset, memoryOffset, storageAdapter, snapshot);
-//     }
-//     ;
-// }
--- apps/sample-app/src/eventstore/PointsStream/SumView.ts
+++ apps/sample-app/src/eventstore/PointsStream/SumView.ts
@@ -19,29 +19,3 @@ export default createViewFactory<SumViewState, PointsStreamEvents>({
         }
     }
 });
-
-// export default class SumView extends EvDbView<SumViewState> implements IEvDbViewAppliesSet<SumViewState, PointsStreamEvents> {
-//     applyPointsAdded(oldState: SumViewState, newEvent: PointsAdded) {
-//         const newState = new SumViewState(oldState.sum + newEvent.points);
-//         return newState;
-//     };
-//     applyPointsSubtracted(oldState: SumViewState, newEvent: PointsSubtracted, eventMetadata: IEvDbEventMetadata) { return new SumViewState(oldState.sum - newEvent.points) };
-
-//     public getDefaultState(): SumViewState {
-//         return new SumViewState();
-//     }
-
-//     constructor(
-//         streamId: string,
-//         storageAdapter: IEvDbStorageSnapshotAdapter,
-//         storedAt: Date | undefined = undefined,
-//         snapshot: EvDbStoredSnapshotResult<SumViewState> = EvDbStoredSnapshotResult.getEmptyState<SumViewState>(),
-//         storeOffset: number = 0,
-//         memoryOffset: number = 0,
-//     ) {
-//         const streamAddress = new EvDbStreamAddress('ExampleStream', streamId);
-//         const viewAddress = new EvDbViewAddress(streamAddress, 'SumView');
-//         super(viewAddress, storedAt, storeOffset, memoryOffset, storageAdapter, snapshot);
-//     }
-//     ;
-// }
