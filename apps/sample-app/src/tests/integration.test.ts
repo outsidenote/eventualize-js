c98bd49d076496c0f18776fb9b025a550822a143
Roma Bronstein <outsidenote@gmail.com>
2025-12-23 15:19:14 +0200

    pass race condition test

--- apps/sample-app/src/tests/integration.test.ts
+++ apps/sample-app/src/tests/integration.test.ts
@@ -30,10 +30,20 @@ describe('Relational Databases Integration Tests', () => {
         Steps.compareFetchedAndStoredStreams(testData.pointsStream, testData.fetchedStream);
       })
 
-      t.test('Duplicate stream cannot be stored', async () => {
+      t.test('AND: Duplicate stream cannot be stored', async () => {
         testData.dupPointsStream = Steps.createPointsStream(testData.streamId, testData.eventStore);
         Steps.addPointsEventsToStream(testData.dupPointsStream);
         await assert.rejects(testData.dupPointsStream.store(), { message: 'OPTIMISTIC_CONCURRENCY_VIOLATION' });
+      })
+
+      t.test('Race condition is handled correctly', async () => {
+        testData.fetchedStream1 = await testData.eventStore.getStream("PointsStream", testData.streamId);
+        testData.fetchedStream2 = await testData.eventStore.getStream("PointsStream", testData.streamId);
+        Steps.addPointsEventsToStream(testData.fetchedStream1);
+        Steps.addPointsEventsToStream(testData.fetchedStream2);
+        const results = await Promise.allSettled([testData.fetchedStream1.store(), testData.fetchedStream2.store()])
+        assert.strictEqual(results.filter(r => r.status === 'fulfilled').length, 1);
+        assert.strictEqual(results.filter(r => r.status === 'rejected').length, 1);
       })
 
       t.after(async () => {
